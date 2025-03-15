a8ef57684e474a2355931685c7b58302daf2c3cc
Maria Loskutova <110923631+minstradamuss@users.noreply.github.com>
2025-03-15 22:31:51 +0300

    Add C code

---
+++ lab05/C_client.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8 -*-
+
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+client.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+port = 12345
+client.bind(("", port))
+
+print(f"UDP Client слушает широковещательные сообщения на порту {port}...")
+
+while True:
+    data, addr = client.recvfrom(1024)
+    print(f"Получено время от {addr}: {data.decode()}")
---
+++ lab05/C_server.py
@@ -0,0 +1,21 @@
+﻿# -*- coding: utf-8 -*-
+
+import time
+import socket
+
+server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+
+server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+port = 12345
+server.bind(("", port))
+
+print(f"UDP Broadcast Server запущен на порту {port}")
+
+while True:
+    current_time = time.ctime()
+    server.sendto(current_time.encode(), ('<broadcast>', port))
+    print(f"Отправлено сообщение: {current_time}")
+    time.sleep(1)
